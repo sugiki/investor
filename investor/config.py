91d3a638cb6655e59f8780101215fbc3d8e6266a
sugiki <sugiki@mercari.com>
2018-01-05 17:57:08 +0900

    change db config

--- investor/config.py
+++ investor/config.py
@@ -6,4 +6,4 @@ LIB_DIR = join(ROOT_DIR, 'investor')
 TMP_DIR = join(ROOT_DIR, 'tmp')
 
 
-SQLALCHEMY_ENGINE = 'mysql+mysqldb://root:pass@127.0.0.1:3336/investor'
+SQLALCHEMY_ENGINE = 'mysql+mysqldb://root:pass@127.0.0.1:3306/investor'
